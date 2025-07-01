3e22b00d7603db4dea6de9b17244965b4a930aeb
nishant01k <merobook@gmail.com>
2025-07-01 12:01:31 +0545

    01k 11

--- app/src/main/java/com/viwcompany/explorenepal/Activity/RegisterActivity.java
+++ app/src/main/java/com/viwcompany/explorenepal/Activity/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.viwcompany.explorenepal.Activity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,19 +10,44 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.viwcompany.explorenepal.R;
+import com.viwcompany.explorenepal.databinding.ActivityRegisterBinding;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private final int[] avatars = {
+            R.drawable.avatar1, R.drawable.avatar2, R.drawable.avatar3,
+            R.drawable.avatar4, R.drawable.avatar5, R.drawable.avatar6,
+            R.drawable.avatar7 ,R.drawable.avatar8 ,R.drawable.avatar9,
+            R.drawable.avatar10
+    };
+
+
+    private CircleImageView userAvatar;
+    private SharedPreferences sharedPreferences;
+
+    private ActivityRegisterBinding binding;
+
+    private FirebaseAuth firebaseAuth;
+    private SweetAlertDialog progressDialog;
+    private CheckBox ch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register);
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
 }
