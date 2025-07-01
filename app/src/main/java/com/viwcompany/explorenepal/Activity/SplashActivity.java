328541103b84e9c879c30b6eb9567f4a4360f836
nishant01k <merobook@gmail.com>
2025-07-01 10:39:12 +0545

    01k 6

--- app/src/main/java/com/viwcompany/explorenepal/Activity/SplashActivity.java
+++ app/src/main/java/com/viwcompany/explorenepal/Activity/SplashActivity.java
@@ -1,26 +1,79 @@
 package com.viwcompany.explorenepal.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.viwcompany.explorenepal.MainActivity;
 import com.viwcompany.explorenepal.R;
+import com.viwcompany.explorenepal.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private ActivitySplashBinding binding;
+    private static final String TAG = "SplashActivity";
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash);
+
+        setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        new Handler().postDelayed(this::checkUser, 3000);
+
+    }
+
+    private void checkUser() {
+            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+            if (firebaseUser == null) {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            } else {
+                DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+                ref.child(firebaseUser.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        String userType = snapshot.child("userType").getValue(String.class);
+                        Intent intent;
+                        if ("user".equals(userType)) {
+                            intent = new Intent(SplashActivity.this, DashboardUserActivity.class);
+                        } else if ("admin".equals(userType)) {
+                            intent = new Intent(SplashActivity.this, DashboardAdminActivity.class);
+                        } else {
+                            intent = new Intent(SplashActivity.this, MainActivity.class);
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Log.e(TAG, "DatabaseError: " + error.getMessage());
+                        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                        finish();
+                    }
+                });
+            }
     }
 }
