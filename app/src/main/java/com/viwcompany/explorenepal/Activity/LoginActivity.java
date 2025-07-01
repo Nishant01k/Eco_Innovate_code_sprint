74062558a3e71e253df832cd28f0a975f82420d6
nishant01k <merobook@gmail.com>
2025-07-01 11:42:29 +0545

    01k 10

--- app/src/main/java/com/viwcompany/explorenepal/Activity/LoginActivity.java
+++ app/src/main/java/com/viwcompany/explorenepal/Activity/LoginActivity.java
@@ -1,18 +1,36 @@
 package com.viwcompany.explorenepal.Activity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.CheckBox;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.viwcompany.explorenepal.R;
 import com.viwcompany.explorenepal.databinding.ActivityLoginBinding;
+
+import java.util.Objects;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
@@ -46,5 +64,178 @@ public class LoginActivity extends AppCompatActivity {
         });
 
 
+        ch = findViewById(R.id.ch2);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
+        progressDialog.setCancelable(false);
+
+
+        binding.noAccountTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+        binding.forgotTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, ForgotPasswordActivity.class));
+            }
+        });
+
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ch.isChecked()) {
+
+                    validateData();
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "Accept the Privacy Policy!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        binding.text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://sites.google.com/view/privacy-policy-for-earnify/home?authuser=8"; // Replace with your desired URL
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(android.net.Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+        binding.forgotTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, ForgotPasswordActivity.class));
+            }
+        });
+
+        binding.contactbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://www.facebook.com/santosh01k";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void showSuccessDialog() {
+        SweetAlertDialog successDialog = new SweetAlertDialog(LoginActivity.this, SweetAlertDialog.SUCCESS_TYPE);
+        successDialog.setTitleText("Success");
+        successDialog.setContentText("Welcome!");
+        successDialog.setConfirmText("Thank You");
+        successDialog.setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+            @Override
+            public void onClick(SweetAlertDialog sweetAlertDialog) {
+                startActivity(new Intent(LoginActivity.this,LoginActivity.class));
+            }
+        });
+        successDialog.setCancelable(false);
+        successDialog.show();
+    }
+
+
+    private String email = "" , password = "";
+
+    private void validateData() {
+
+        email = binding.emailEt.getText().toString().trim();
+        password = binding.passwordEt.getText().toString().trim();
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+
+            Toast.makeText(this, "Invalid email pattern...!", Toast.LENGTH_SHORT).show();
+
+            //SweetToast.custom(this, "Invalid email pattern...!", R.drawable.logo, R.color.white, R.drawable.toasr_backgroung, 10000);
+        }
+        else if (TextUtils.isEmpty(password)) {
+
+            Toast.makeText(this, "Enter Password...!", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            loginUser();
+        }
+    }
+
+    private void loginUser() {
+        progressDialog.setTitleText("Logging In...");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        checkUser();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Toast.makeText(LoginActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+    }
+
+
+
+
+    private void checkUser() {
+        progressDialog.setTitleText("Checking User...");
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        ref.child(firebaseUser.getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        progressDialog.dismiss();
+                        String userType = "" + snapshot.child("userType").getValue();
+                        if (userType.equals("user")) {
+                            if(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).isEmailVerified()){
+                                progressDialog.dismiss();
+                                Toast.makeText(LoginActivity.this, "Login Successfully...", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(LoginActivity.this, DashboardUserActivity.class));
+                                finish();
+                            }
+                            else {
+                                Toast.makeText(LoginActivity.this, "Please Verify your Email First", Toast.LENGTH_SHORT).show();
+                            }
+                        } else if (userType.equals("admin")) {
+
+
+                            if (Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).isEmailVerified()) {
+                                progressDialog.dismiss();
+                                Toast.makeText(LoginActivity.this, "Login Successfully....", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(LoginActivity.this, DashboardAdminActivity.class));
+                                finish();
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Please Verify your Email First", Toast.LENGTH_SHORT).show();
+                            }
+                        }else{
+                                Toast.makeText(LoginActivity.this, "Please Verify your Email First", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        moveTaskToBack(false);
     }
 }
